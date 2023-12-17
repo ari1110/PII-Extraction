c9e74dc96fa47af2bd13f3f3ab99396d18058c32
ari1110 <37885347+ari1110@users.noreply.github.com>
2023-12-17 00:03:06 +0000

    Adjusting the entry code - getting used to the stuff

--- tagger_parser_ud/Add'l_file.py
+++ tagger_parser_ud/Add'l_file.py
@@ -1,12 +1,22 @@
-import sys
-import spacy
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
 
-if 'spacy' in sys.modules:
-    print("spacy is already imported")
-else:
-    print("spacy is not imported")
+text="My phone number is 212-555-5555, and my name is Luke. I'm from NYC, but I've grown up in Brighton. My sales rep DanClaus is the best."
 
-# Use spacy to load an English model and process a text
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a sentence.")
-print([token.text for token in doc])
+# Set up the engine, loads the NLP module (spaCy model by default) 
+# and other PII recognizers
+analyzer = AnalyzerEngine()
+
+# Call analyzer to get results
+results = analyzer.analyze(text=text, language="en",
+    # entities=["PHONE_NUMBER"]
+)
+print(results)
+
+# Analyzer results are passed to the AnonymizerEngine for anonymization
+
+anonymizer = AnonymizerEngine()
+
+anonymized_text = anonymizer.anonymize( text=text, analyzer_results=results)
+
+print(anonymized_text)
